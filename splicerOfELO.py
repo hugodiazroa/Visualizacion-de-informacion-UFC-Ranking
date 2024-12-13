0f8ff5e8f79c5372964596a5ea7391b0d3a79655
hugodiazroa <hugodiazroa@gmail.com>
2024-12-13 05:30:39 -0300

    new file to calculate the elo of fighters or splice a database that has the fight-to-fight elo ranking

---
+++ splicerOfELO.py
@@ -0,0 +1,17 @@
+#in this file we will read the data from the csv file "large_dataset.csv"
+#we will output just the data that we need for the "report.ipynb" file into a "elo.csv" file
+#we will create a new csv file "elo.csv" that will contain the following columns:
+"""
+1. "id" - the id of the point to plot (a hash of the name of the player and the date of the fight)
+2. "result" - the result of the fight (1 if the player won, 0 if the player lost)
+3. "date" - the date of the fight
+4. "name" - the name of the player
+5. "elo" - the original elo of the player
+6. "elo_opp" - the original elo of the opponent
+7. "elo_new" - the new elo of the player (this is redundant, but it will make it easier to plot the data)
+8. "elo_opp_new" - the new elo of the opponent (this is redundant, but it will make it easier to plot the data)
+9. "HD" - the HD rank of the player (
+"""
+
+import pandas as pd
+import numpy
